7cd0b7be91717881dc19e15b989917a36e3e662f
Hereugo <thereug6@gmail.com>
2021-05-09 17:40:05 +0600

    make it better

--- app.py
+++ app.py
@@ -246,7 +246,11 @@ def ask_question(message):
 	userId = message.chat.id
 	currentInlineState = [keyFormat, keyFormat]
 	keyboard = create_keyboard(tree.ask_question.buttons, currentInlineState)
-	bot.send_message(userId, tree.ask_question.text, reply_markup=keyboard)
+
+	bot.send_photo(chat_id=userId,
+				   photo=tree.ask_question.image,
+				   caption=tree.ask_question.text,
+				   reply_markup=keyboard)
 def question(message):
 	userId = message.chat.id
 	currentInlineState = [keyFormat]
